143407c148c530aa33a9770b891044ffaed377e3
Luka Jerković <lujerkovic@gmail.com>
2023-11-24 16:56:07 +0100

    Added dark theme and toggle button

--- src/app/notes/NavBar.tsx
+++ src/app/notes/NavBar.tsx
@@ -8,8 +8,12 @@ import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import { useState } from "react";
 import AddEditNoteDialog from "@/components/AddEditNoteDialog";
+import ThemeToggleButton from "@/components/ThemeToggleButton";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 export default function NavBar() {
+  const { theme } = useTheme();
   const [showAddEditNoteDialog, setShowAddEditNoteDialog] = useState(false);
   return (
     <>
@@ -20,16 +24,19 @@ export default function NavBar() {
             <span className="font-bold">FlowBrain</span>
           </Link>
           <div className="flex items-center gap-2">
-            <UserButton
-              afterSignOutUrl="/"
-              appearance={{
-                elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
-              }}
-            />
+            <ThemeToggleButton />
             <Button onClick={() => setShowAddEditNoteDialog(true)}>
               <Plus size={20} className="mr-2" />
               Add Note
             </Button>
+
+            <UserButton
+              afterSignOutUrl="/"
+              appearance={{
+                baseTheme: theme === "dark" ? dark : undefined,
+                elements: { avatarBox: { width: "2.5rem", height: "2.5rem" } },
+              }}
+            />
           </div>
         </div>
       </div>
